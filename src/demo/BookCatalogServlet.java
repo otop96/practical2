0377e5865136ddf1ae18d5ada62cf9f1effb49e8
152107W <152107W@mymail.nyp.edu.sg>
2016-10-24 17:40:07 +0800

    Initial commit

---
+++ src/demo/BookCatalogServlet.java
@@ -0,0 +1,22 @@
+package demo;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by 152107W on 10/24/2016.
+ */
+@WebServlet(name = "BookCatalogServlet")
+public class BookCatalogServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
